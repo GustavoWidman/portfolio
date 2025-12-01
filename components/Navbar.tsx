1194e8fe915186a4866a7b2f94d60fc3d16dc399
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-11-30 22:43:52 -0300

    feat: revert broken ResumeButton padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -87,7 +87,7 @@ const Navbar: React.FC<NavbarProps> = ({ scrollY, lang, setLang, theme, setTheme
 
               <div className="flex items-center gap-4 pl-4 border-l border-zinc-200 dark:border-zinc-800">
                 {/* Resume Button Dropdown */}
-                <ResumeButton align="center" position="bottom" className="pt-2">
+                <ResumeButton align="center" position="bottom">
                   <div className="p-2 rounded-full hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-colors text-zinc-600 dark:text-zinc-400 group relative">
                     <FileText size={18} />
                     <span className="absolute top-full left-1/2 -translate-x-1/2 mt-2 px-2 py-1 text-[10px] bg-black text-white rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap pointer-events-none">
